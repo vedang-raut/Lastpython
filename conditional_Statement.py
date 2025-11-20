f78a7b46ff3dbea0f4b5a30d7331f8c4d20ba7a0
Vedang <vedangraut1123@gmail.com>
2025-11-20 15:20:41 +0530

    this commit has conditional statement

---
+++ conditional_Statement.py
@@ -0,0 +1,10 @@
+
+
+#Wap to check the number is multiple of 7 or not
+
+num=int(input("Enter the number to check:"))
+
+if (num%7==0):
+    print("Yes it is the mumltiple of 7")
+else:
+    print("Not multiple of 7")
--- operators.py
+++ operators.py
@@ -29,6 +29,8 @@ num %=5
 num **=2
 print(num)
 
+
+
 #logical operators
 a=True
 b=False
